9f9b91a87639a93307f8ab6c180ee4b5f8d374d7
Seth Berrier <berriers@uwstout.edu>
2022-06-24 15:08:13 -0500

    Added server nickname to logging

--- server/util/logging.js
+++ server/util/logging.js
@@ -3,6 +3,7 @@ import path from 'path'
 
 // Bring in Winston logging utilities
 import * as winston from 'winston'
+import { format } from 'logform'
 import LogzioWinstonTransport from 'winston-logzio'
 import ExpressWinston from 'express-winston'
 import 'winston-daily-rotate-file'
@@ -12,6 +13,7 @@ import dotenv from 'dotenv'
 
 // Read in secret tokens and config
 dotenv.config()
+const HOST_NICKNAME = process.env.HOST_NICKNAME || 'unknown'
 const LOGZIO_TOKEN = process.env.LOGZIO_TOKEN || 'bad-token'
 const LOGZIO_LISTENER = process.env.LOGZIO_LISTENER || 'nowhere.com'
 
@@ -19,11 +21,20 @@ const LOGZIO_LISTENER = process.env.LOGZIO_LISTENER || 'nowhere.com'
 const _DEV_ = process.argv.find((arg) => (arg.toLowerCase() === 'dev' || arg.toLowerCase() === 'development'))
 const verbose = process.argv.find((arg) => (arg.toLowerCase() === 'verbose'))
 
+// Special format to add the host nickname
+const serverName = format((info, opts) => {
+  if (opts.serverName) {
+    info.serverName = opts.serverName
+  }
+  return info
+})
+
 // Different formats for different log transports
 const formatBase = [
   winston.format.timestamp(),
   winston.format.ms(),
-  winston.format.printf(info => `${info.timestamp} ${info.ms.padEnd(8, ' ')} ${info.level.padEnd(8, ' ')} [${info.label}] ${info.message}`)
+  winston.format.padLevels(),
+  winston.format.printf(info => `${info.timestamp} ${info.ms.padEnd(8, ' ')} ${`[${info.label}]`.padEnd(25, ' ')} ${info.level} ${info.message}`)
 ]
 const humanTextLogFormat = (colorize = true) => (
   (colorize
@@ -52,10 +63,10 @@ const logzioWinstonTransport = new LogzioWinstonTransport({
 // Daily rotating file transport
 const rotateFileSharedConfig = {
   level: (verbose ? 'verbose' : 'info'),
-  datePattern: 'MM-DD-YYYY-HH',
+  datePattern: 'MM-DD-YYYY',
   zippedArchive: true,
   maxSize: '20m',
-  maxFiles: '14d'
+  maxFiles: '7d'
 }
 
 // Local rotating file for 'info' and higher messages
@@ -96,7 +107,10 @@ const allFileTransports = [
 
 // Construct a logger config with our intended format and transports
 const makeConfig = (label) => ({
-  format: winston.format.label({ label }),
+  format: winston.format.combine(
+    winston.format.label({ label }),
+    serverName({ serverName: HOST_NICKNAME })
+  ),
   transports: (
     _DEV_
       ? [
