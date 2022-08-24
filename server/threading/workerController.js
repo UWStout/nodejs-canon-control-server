09754648ba6107edb4b0beeeaf231ebfdbea5779
Seth Berrier <berriers@uwstout.edu>
2022-08-24 11:19:39 -0500

    Adding thread pool size to env

--- server/threading/workerController.js
+++ server/threading/workerController.js
@@ -1,27 +1,29 @@
 import path from 'path'
 import WorkerWrapper from './WorkerWrapper.js'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 // Hardcoded path to script (relative to working directory)
 const IMG_DOWNLOAD_SCRIPT_PATH = path.join(process.cwd(), 'server', 'threading', 'imageDownloadWorker.js')
 
 // Initialize Worker Pool
-const POOL_SIZE = 16
+const THREAD_POOL_SIZE = process.env.THREAD_POOL_SIZE || 4
 const workerPool = []
-for (let i = 0; i < POOL_SIZE; ++i) { workerPool.push(new WorkerWrapper(IMG_DOWNLOAD_SCRIPT_PATH, rePoolWorker)) }
+for (let i = 0; i < THREAD_POOL_SIZE; ++i) { workerPool.push(new WorkerWrapper(IMG_DOWNLOAD_SCRIPT_PATH, rePoolWorker)) }
 
 const taskQueue = []
 
 // Add Worker back to pool and check for new tasks
-function rePoolWorker(worker) {
+function rePoolWorker (worker) {
   workerPool.push(worker)
   doNextTask()
 }
 
 // Pop next task, If a worker is available, assign the task, otherwise reQueue the task
-function doNextTask() {
+function doNextTask () {
   const nextTask = taskQueue.pop()
-  if (nextTask !== undefined)
-  {
+  if (nextTask !== undefined) {
     const nextWorker = workerPool.pop()
     if (nextWorker !== undefined) {
       nextWorker.runWorker(nextTask)
@@ -33,6 +35,6 @@ function doNextTask() {
 
 // Add new task to queue and check for available workers
 export async function downloadImgThreaded (imgData, filePath, imgInfoCallback) {
-    taskQueue.push({workerData: {imgData, filePath}, imgInfoCallback})
-    doNextTask()
+  taskQueue.push({ workerData: { imgData, filePath }, imgInfoCallback })
+  doNextTask()
 }
